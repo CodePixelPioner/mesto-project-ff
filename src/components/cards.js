30f5360ccd63c60b8457d07eb6a31a8909fce454
CodePixelPioner <xoxlunowa.o@yandex.ru>
2024-03-26 15:08:03 +0300

    Configuring the Webpack Build

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,4 +1,4 @@
-const initialCards = [
+export const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
@@ -13,7 +13,7 @@ const initialCards = [
     },
     {
       name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg"
     },
     {
       name: "Холмогорский район",
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
+// index.js
+
+import '../pages/index.css'; //импорт главного файла стилей
+import { initialCards } from './cards.js';
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
